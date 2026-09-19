ff247eab725c0ff8a9cbccea073c00d4592d2b55
jakob611 <195969440+jakob611@users.noreply.github.com>
2026-09-19 05:54:55 +0000

    compile fix: manjkajoci import Dp v RoutineApp (spatialSpec<Dp>)

--- app/src/main/java/com/example/mydailyroutine/app/presentation/RoutineApp.kt
+++ app/src/main/java/com/example/mydailyroutine/app/presentation/RoutineApp.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.hapticfeedback.HapticFeedback
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
